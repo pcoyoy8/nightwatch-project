5fa93be7f2e0dbf0b7e5c46d2f47993f99bf4c4c
Pablo Coyoy <pablo@oaky.com>
2020-12-02 20:56:20 +0100

    Adding a first test

--- tests/myFirstTests.js
+++ tests/myFirstTests.js
@@ -1,7 +1,9 @@
-const moment = require('moment');
-
-const nameInput = 'input[data-test=name]';
-const dobInput = 'input[data-test=dob]';
+const nameInput = 'input[data-test="name"]';
+const dobInput = 'input[data-test="dob-field"]';
+const greetingInput = 'input[data-test="greeting"';
+const jokeButton = 'button[data-test="joke-button"]';
+const jokeText = 'textarea[data-test="joke-text"]';
+const name = 'Joe Doe';
 
 describe('Nightwatch tests', () => {
   test('My first e2e test', (browser) => {
@@ -9,16 +11,33 @@ describe('Nightwatch tests', () => {
       .url('http://localhost:3000/')
       .waitForElementVisible('body');
 
-    const name = 'Mario Josue';
-    const date = moment().toDate();
-
     browser
       .assert.visible(nameInput)
       .setValue(nameInput, name)
       .assert.value(nameInput, name)
       .assert.visible(dobInput)
-      .setValue(dobInput, date)
-      .assert.value(dobInput, date);
+      .click(dobInput)
+      .waitForElementVisible('div[class="v-date-picker-title"]');
+
+    for (let index = 0; index <= 216; index += 1) {
+      browser
+        .click('button[aria-label="Previous month"]');
+    }
+
+    browser
+      .useXpath()
+      .waitForElementVisible('//div[normalize-space(text())="16"]')
+      .click('//div[normalize-space(text())="16"]')
+      .pause(999);
+
+    browser
+      .useCss()
+      .waitForElementVisible(greetingInput)
+      .assert.value(greetingInput, `Welcome ${name}, ahh! You are 18 years old, so you can see a joke`)
+      .assert.visible(jokeButton)
+      .click(jokeButton)
+      .assert.visible(jokeText)
+      .assert.not.value(jokeText, '');
 
     browser.end();
   });
